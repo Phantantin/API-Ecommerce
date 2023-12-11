ef11cdc19385deba4ba5c92fa29724d8c0ee17c5
Phantantin <101706282+Phantantin@users.noreply.github.com>
2023-12-11 22:42:39 +0700

    Update cloudinary.js

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -34,6 +34,7 @@ cloudinary.v2.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
     api_secret: process.env.SECRET_KEY,
+    secure: true
   });
 
 const cloudinaryUploadImg = async (fileToUploads) => {
