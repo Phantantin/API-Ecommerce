102609394a21b273fcd571868b211789a4cf71b1
Phantantin <101706282+Phantantin@users.noreply.github.com>
2023-12-11 22:39:37 +0700

    Update cloudinary.js

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -30,7 +30,7 @@
 const cloudinary = require('cloudinary');
 
 
-cloudinary.config({
+cloudinary.v2.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.API_KEY,
     api_secret: process.env.SECRET_KEY,
