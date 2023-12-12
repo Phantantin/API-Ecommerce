214b87470c9869c4bc300712c9a1a01f56ef03e7
Phan Tấn Tín <tantin10082003@gmail.com>
2023-12-12 13:03:46 +0700

    Api

--- middlewares/uploadImages.js
+++ middlewares/uploadImages.js
@@ -38,6 +38,7 @@ const productImgResize = async(req, res, next) =>{
                 .toFormat("jpeg")
                 .jpeg({quality: 90})
                 .toFile(`public/images/products/${file.filename}`);
+            fs.existsSync(`public/images/products/${file.filename}`);
             fs.unlinkSync(`public/images/products/${file.filename}`);
         })
         
@@ -54,6 +55,7 @@ const blogImgResize = async(req, res, next) =>{
                 .toFormat("jpeg")
                 .jpeg({quality: 90})
                 .toFile(`public/images/blogs/${file.filename}`);
+            fs.existsSync(`public/images/blogs/${file.filename}`);
             fs.unlinkSync(`public/images/blogs/${file.filename}`);
         })
     );
