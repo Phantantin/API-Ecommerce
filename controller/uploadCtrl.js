3396e3651dd7153c821764b000fb7e8c3d7c63a9
Phan Tấn Tín <tantin10082003@gmail.com>
2023-12-12 14:46:22 +0700

    Api

--- controller/uploadCtrl.js
+++ controller/uploadCtrl.js
@@ -1,55 +1,28 @@
 const {cloudinaryUploadImg, cloudinaryDeleteImg} = require('../utils/cloudinary');
 const fs = require('fs');
 const asyncHandler = require('express-async-handler');
-// 
-// const uploadImages = asyncHandler(async( req, res) =>{
-//     try {
-//         const uploader = (path) => cloudinaryUploadImg(path, "images");
-//         const urls = [];
-//         const files = req.files;
-//         for(const file of files){
-//             const {path} = file;
-//             const newpath = await uploader(path);
-//             console.log(newpath);
-//             urls.push(newpath);
-//             fs.unlinkSync(path);
-//         }
-//         const images = urls.map((file) =>{
-//             return file;
-//         });
-//         res.json(images);
-//     } catch (error) {
-//         throw new Error(error)
-//     }
-// });
 
-
-const uploadImages = asyncHandler(async (req, res) => {
+const uploadImages = asyncHandler(async( req, res) =>{
     try {
         const uploader = (path) => cloudinaryUploadImg(path, "images");
         const urls = [];
         const files = req.files;
-        for (const file of files) {
-            const { path } = file;
+        for(const file of files){
+            const {path} = file;
             const newpath = await uploader(path);
             console.log(newpath);
             urls.push(newpath);
-
-            // Kiểm tra xem tệp tin có tồn tại trước khi xóa
-            if (fs.existsSync(path)) {
-                fs.unlinkSync(path);
-            } else {
-                console.log(`File ${path} không tồn tại`);
-            }
+            fs.unlinkSync(path);
         }
-        const images = urls.map((file) => {
+        const images = urls.map((file) =>{
             return file;
         });
         res.json(images);
     } catch (error) {
-        throw new Error(error);
+        throw new Error(error)
     }
 });
+
 
 
 //
--- middlewares/uploadImages.js
+++ middlewares/uploadImages.js
@@ -3,6 +3,8 @@ const sharp = require('sharp');
 const path = require('path');
 const fs = require('fs');
 
+const os = require('os');
+
 const multerStorage = multer.diskStorage({
     destination: function(req,file, cb){
         cb(null, path.join(__dirname, "../public/images/"));
@@ -29,18 +31,39 @@ const uploadPhoto = multer({
     limits: {fieldSize: 2000000},
 });
 
-const productImgResize = async(req, res, next) =>{
-    if(!req.files) return next();
+const tempDir = os.tmpdir(); // Tạo thư mục tạm thời
+
+// const productImgResize = async(req, res, next) =>{
+//     if(!req.files) return next();
+//     await Promise.all(
+//         req.files.map(async(file) =>{
+//             await sharp(file.path)
+//                 .resize(300, 300)
+//                 .toFormat("jpeg")
+//                 .jpeg({quality: 90})
+//                 .toFile(`public/images/products/${file.filename}`);
+//             // fs.unlinkSync(`public/images/products/${file.filename}`);
+//         })
+        
+//     );
+//     next();
+// };
+
+const productImgResize = async (req, res, next) => {
+    if (!req.files) return next();
     await Promise.all(
-        req.files.map(async(file) =>{
+        req.files.map(async (file) => {
+            const tempFilePath = path.join(tempDir, file.filename); // Lưu ảnh tạm thời vào thư mục tạm
             await sharp(file.path)
                 .resize(300, 300)
-                .toFormat("jpeg")
-                .jpeg({quality: 90})
-                .toFile(`public/images/products/${file.filename}`);
-            // fs.unlinkSync(`public/images/products/${file.filename}`);
+                .toFormat('jpeg')
+                .jpeg({ quality: 90 })
+                .toFile(tempFilePath);
+
+            const uploadResult = await cloudinaryUploadImg(tempFilePath, 'images'); // Upload ảnh từ thư mục tạm lên Cloudinary
+
+            fs.unlinkSync(tempFilePath); // Xóa ảnh tạm thời sau khi upload lên Cloudinary
         })
-        
     );
     next();
 };
