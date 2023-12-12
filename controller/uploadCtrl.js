853beea1b7e6022ee555acf97de6861564cd3c8e
Phan Tấn Tín <tantin10082003@gmail.com>
2023-12-12 15:51:16 +0700

    Api

--- controller/uploadCtrl.js
+++ controller/uploadCtrl.js
@@ -2,26 +2,6 @@ const {cloudinaryUploadImg, cloudinaryDeleteImg} = require('../utils/cloudinary'
 const fs = require('fs');
 const asyncHandler = require('express-async-handler');
 
-// const uploadImages = asyncHandler(async( req, res) =>{
-//     try {
-//         const uploader = (path) => cloudinaryUploadImg(path, "images");
-//         const urls = [];
-//         const files = req.files;
-//         for(const file of files){
-//             const {path} = file;
-//             const newpath = await uploader(path);
-//             console.log(newpath);
-//             urls.push(newpath);
-//             fs.unlinkSync(path);
-//         }
-//         const images = urls.map((file) =>{
-//             return file;
-//         });
-//         res.json(images);
-//     } catch (error) {
-//         throw new Error(error)
-//     }
-// });
 
 const uploadImages = async (req, res) => {
     try {
--- middlewares/uploadImages.js
+++ middlewares/uploadImages.js
@@ -35,21 +35,6 @@ const uploadPhoto = multer({
 
 const tempDir = os.tmpdir(); // Tạo thư mục tạm thời
 
-// const productImgResize = async(req, res, next) =>{
-//     if(!req.files) return next();
-//     await Promise.all(
-//         req.files.map(async(file) =>{
-//             await sharp(file.path)
-//                 .resize(300, 300)
-//                 .toFormat("jpeg")
-//                 .jpeg({quality: 90})
-//                 .toFile(`public/images/products/${file.filename}`);
-//             // fs.unlinkSync(`public/images/products/${file.filename}`);
-//         })
-        
-//     );
-//     next();
-// };
 
 const productImgResize = async (req, res, next) => {
     if (!req.files) return next();
