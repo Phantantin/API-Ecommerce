8322c8904f8fa6f8f77a4e6b03f9bed7b63bd069
Phan Tấn Tín <tantin10082003@gmail.com>
2023-12-12 17:04:27 +0700

    Api

--- middlewares/uploadImages.js
+++ middlewares/uploadImages.js
@@ -7,7 +7,14 @@ const os = require('os');
 
 
 const tempDir = os.tmpdir(); // Đường dẫn thư mục tạm thời
-const uploadDir = path.join(__dirname, "../public/images/"); // Đường dẫn thư mục upload
+// const uploadDir = path.join(__dirname, "../public/images/"); // Đường dẫn thư mục upload
+
+const uploadDir = 'D:/Ecommerce-Deploy/backend/public/images/';
+
+// Kiểm tra và tạo thư mục nếu chưa tồn tại
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const multerStorage = multer.diskStorage({
     destination: function(req, file, cb) {
